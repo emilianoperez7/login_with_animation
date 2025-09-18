dddef812f64b0dfc16756c31824e88207cf95c2d
emilianojesusp599@gmail.com <emilianojesusp599@gmail.com>
2025-09-18 10:54:22 -0600

    feat(rive): wire up isChecking/isHandsUp inputs on login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,9 +9,19 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  //Controlar si la contraseña se ve o no
   bool _isObscure = true;
+  //Cerebro de la logica de las animaciones
+  StateMachineController? controller;
+  //SMI= Statae Machine Input
+  SMIBool? isChecking; //Activa el modo chismoso
+  SMIBool? isHandsUp; //Se tapa los ojos
+  SMITrigger? trigSuccess; // Se emociona
+  SMITrigger? trigFail; // Se pone sad
+
   @override
   Widget build(BuildContext context) {
+    //Consulta el tamaño de la pantalla
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
         body: SafeArea(
@@ -23,12 +33,35 @@ class _LoginScreenState extends State<LoginScreen> {
             SizedBox(
                 width: size.width,
                 height: 200,
-                child:
-                    RiveAnimation.asset('assets/animated_login_character.riv')),
+                child: RiveAnimation.asset(
+                    'assets/animated_login_character.riv',
+                    stateMachines: ["Login Machine"],
+                    //Al iniciarse
+                    onInit: (artboard) {
+                  controller = StateMachineController.fromArtboard(
+                      artboard, "Login Machine");
+                  //Verificar que inició bien
+                  if (controller == null) return;
+                  artboard.addController(controller!);
+                  //Asignar las variables
+                  isChecking = controller!.findSMI("isChecking");
+                  isHandsUp = controller!.findSMI("isHandsUp");
+                  trigSuccess = controller!.findSMI("trigSuccess");
+                  trigFail = controller!.findSMI("trigFail");
+                })),
             //Espacio entre el oso y el texto email
             const SizedBox(height: 10),
             //Campo de texto email
             TextField(
+              onChanged: (value) {
+                if (isHandsUp != null) {
+                  //No tapar los ojos al escribir el mail
+                  isHandsUp!.change(false);
+                }
+                if (isChecking == null) return;
+                //Activar el modo chismoso
+                isChecking!.change(true);
+              },
               //Para que el teclado sea de email en móviles
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
@@ -44,6 +77,15 @@ class _LoginScreenState extends State<LoginScreen> {
             const SizedBox(height: 10),
             //Campo de texto password
             TextField(
+              onChanged: (value) {
+                if (isChecking != null) {
+                  //No tapar los ojos al escribir el mail
+                  isChecking!.change(false);
+                }
+                if (isHandsUp == null) return;
+                //Activar el modo chismoso
+                isHandsUp!.change(true);
+              },
               //Para que el teclado sea de password visible en móviles
               keyboardType: TextInputType.visiblePassword,
               obscureText: _isObscure,
@@ -64,6 +106,51 @@ class _LoginScreenState extends State<LoginScreen> {
                       icon: Icon(
                         _isObscure ? Icons.visibility : Icons.visibility_off,
                       ))),
+            ),
+            const SizedBox(height: 10),
+            //Texto "Forgot your password?" alineado a la derecha
+            SizedBox(
+              width: size.width,
+              child: const Text(
+                "¿Forgot your password?",
+                //Alinear a la derecha
+                textAlign: TextAlign.right,
+                style: TextStyle(decoration: TextDecoration.underline),
+              ),
+            ),
+            //Boton de Login
+            const SizedBox(height: 10),
+            //Boton estilo Android
+            MaterialButton(
+              onPressed: () {
+                //TODO:
+              },
+              minWidth: size.width,
+              height: 50,
+              color: Colors.deepPurple,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              child:
+                  const Text("Log In", style: TextStyle(color: Colors.white)),
+            ),
+            const SizedBox(height: 10),
+            SizedBox(
+              width: size.width,
+              child:
+                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                const Text("Don´t have an account?"),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text("Register",
+                      style: TextStyle(
+                          color: Colors.black,
+                          //En negritas
+                          fontWeight: FontWeight.bold,
+                          //Subrayado
+                          decoration: TextDecoration.underline)),
+                  //El STATE MACHINE ES COMO EL CEREBRO QUE DECIDE QUE ANIMACION CORRE Y CUANDO
+                ),
+              ]),
             ),
           ],
         ),
